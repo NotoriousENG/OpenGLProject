75a305b18b924e8167b00051a5d2012aeddc8f68
Michael O'Connell <michaeljoconnell@ufl.edu>
2020-09-30 23:03:44 -0400

    Bouncing ball physics

---
+++ OpenGLProject/MeshRenderer.h
@@ -0,0 +1,31 @@
+#pragma once
+# include <vector>
+
+#include "Camera.h"
+#include "LightRenderer.h"
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <bullet/btBulletDynamicsCommon.h>
+
+class MeshRenderer
+{
+public:
+	MeshRenderer(MeshType modelType, Camera* _camera, btRigidBody* _rigidBody);
+	~MeshRenderer();
+	void draw();
+	void setPosition(glm::vec3 _position);
+	void setScale(glm::vec3 _scale);
+	void setProgram(GLuint _program);
+	void setTexture(GLuint _textureID);
+private:
+	std::vector<Vertex>vertices;
+	std::vector<GLuint>indices;
+	glm::mat4 modelMatrix;
+	Camera* camera;
+	glm::vec3 position, scale;
+	GLuint vao, vbo, ebo, texture, program;
+	btRigidBody* rigidBody;
+};
--- OpenGLProject/OpenGLProject.cpp
+++ OpenGLProject/OpenGLProject.cpp
@@ -2,15 +2,22 @@
 //
 
 #include "OpenGLProject.h"
-#include "GL/glew.h"
-#include "GLFW/glfw3.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <bullet/btBulletDynamicsCommon.h>
+#include <chrono>
 
 #include "ShaderLoader.h"
 #include "Camera.h"
 #include "LightRenderer.h"
+#include "MeshRenderer.h"
+#include "TextureLoader.h"
 
 Camera* camera;
 LightRenderer* light;
+MeshRenderer* sphere;
+MeshRenderer* ground;
+btDiscreteDynamicsWorld* dynamicsWorld;
 
 static void glfwError(int id, const char* description);
 void initGame();
@@ -25,6 +32,29 @@ void initGame()
 {
 	glEnable(GL_DEPTH_TEST);
 
+	//init physics
+	auto* broadphase = new btDbvtBroadphase();
+	auto* collisionConfiguration = new btDefaultCollisionConfiguration();
+	auto* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+	auto* solver = new btSequentialImpulseConstraintSolver();
+
+	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+	dynamicsWorld->setGravity(btVector3(0, -9.8f, 0));
+
+	auto* sphereShape = new btSphereShape(1.0f);
+	auto* sphereMotionState = new
+		btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1),
+			btVector3(0, 10.0f, 0)));
+	btScalar mass = 10.0;
+	btVector3 sphereInertia(0, 0, 0);
+	sphereShape->calculateLocalInertia(mass, sphereInertia);
+	btRigidBody::btRigidBodyConstructionInfo sphereRigidBodyCI(mass,sphereMotionState, sphereShape, sphereInertia);
+	auto* sphereRigidBody = new btRigidBody(sphereRigidBodyCI);
+	sphereRigidBody->setRestitution(1.0f);
+	sphereRigidBody->setFriction(1.0f);
+	dynamicsWorld->addRigidBody(sphereRigidBody);
+
+
 	ShaderLoader shader;
 
 	GLuint flatShaderProgram = 
@@ -32,11 +62,42 @@ void initGame()
 			"Assets/Shaders/FlatModel.vert", 
 			"Assets/Shaders/FlatModel.frag");
 
-	camera = new Camera(45.0f, 800, 600, 0.1f, 100.0f, glm::vec3(0.0f,4.0f, 6.0f));
+	GLuint texturedShaderProgram =
+		shader.createProgram("Assets/Shaders/TexturedModel.vert",
+			"Assets/Shaders/TexturedModel.frag");
+
+	TextureLoader tLoader;
+	GLuint sphereTexture = tLoader.getTextureID("Assets/Textures/globe.jpg");
+
+	camera = new Camera(45.0f, 800, 600, 0.1f, 100.0f, glm::vec3(0.0f,4.0f, 20.0f));
 
 	light = new LightRenderer(MeshType::kCube, camera);
 	light->setProgram(flatShaderProgram);
 	light->setPosition(glm::vec3(0.0f, 0.0f, 0.0f));
+
+	sphere = new MeshRenderer(MeshType::kSphere, camera, sphereRigidBody);
+	sphere->setProgram(texturedShaderProgram);
+	sphere->setTexture(sphereTexture);
+	sphere->setPosition(glm::vec3(0.0f, 0.0f, 0.0f));
+	sphere->setScale(glm::vec3(1.0f));
+
+	btCollisionShape* groundShape = new btBoxShape(btVector3(4.0f,0.5f, 4.0f));
+	auto* groundMotionState = new
+		btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1),
+			btVector3(0, -2.0f, 0)));
+	btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI(0.0f, new btDefaultMotionState(), groundShape, btVector3(0, 0, 0));
+	auto* groundRigidBody = new btRigidBody(groundRigidBodyCI);
+	groundRigidBody->setFriction(1.0);
+	groundRigidBody->setRestitution(0.9);
+	groundRigidBody->setCollisionFlags(btCollisionObject::CF_STATIC_OBJECT);
+	dynamicsWorld->addRigidBody(groundRigidBody);
+	
+	// Ground Mesh
+	GLuint groundTexture = tLoader.getTextureID("Assets/Textures/ground.jpg");
+	ground = new MeshRenderer(MeshType::kCube, camera, groundRigidBody);
+	ground->setProgram(texturedShaderProgram);
+	ground->setTexture(groundTexture);
+	ground->setScale(glm::vec3(4.0f, 0.5f, 4.0f));
 }
 
 void renderScene()
@@ -45,7 +106,9 @@ void renderScene()
 	glClearColor(1, 1, 0, 1);
 
 	// draw game objects here
-	light->draw();
+	// light->draw();
+	sphere->draw();
+	ground->draw();
 }
 
 
@@ -59,13 +122,21 @@ int main(int argc, char **argv)
 
 	glewInit();
 	initGame();
+	auto previousTime = std::chrono::high_resolution_clock::now();
 	while (!glfwWindowShouldClose(window))
 	{
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		float dt = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - previousTime).count();
+
+		dynamicsWorld->stepSimulation(dt);
+		
 		// render our scene
 		renderScene();
 		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+		previousTime = currentTime;
 	}
 	glfwTerminate();
 
